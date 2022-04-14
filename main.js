780469c2144a8b9a733060328bcbcb46ad01368e
Jtdoe <Jordendoerflinger@gmail.com>
2022-04-14 17:59:31 -0500

    passing tests

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const { strictEqual } = require('assert');
 // brings in the assert module for unit testing
 const assert = require('assert');
 // brings in the readline module to access the command line
@@ -12,11 +13,22 @@ const rl = readline.createInterface({
 
 
 const pigLatin = (word) => {
+word = word.trim(' ')
+word = word.toLowerCase()
 let vowels = ['a', 'e', 'i', 'o', 'u']
 
 if (vowels.includes(word[0])) {
-  return word + "yay"
+  return word + "yay" 
+} 
+let constPre = ""
+for (let i = 0; i < word.length; i++) {
+  const char = word[i]
+  if (vowels.includes(char)) {
+    break;
+  }
+  constPre += char;
 }
+return (word.substring(constPre.length) + constPre + "ay")
 }
 
 // the first function called in the program to get an input from the user
@@ -70,4 +82,4 @@ if (typeof describe === 'function') {
 // break your code into pieces and focus on one piece at a time...
 // 1. if word begins with a vowel send to one function: adds "yay"
 // 2. if word begins with a consonant send to another function: splices off beginning, returns word with new ending.
-// 3. if multiple words, create array of words, loop over them, sending them to different functions and creating a new array with the new words.
+// 3. if multiple words, create array of words, loop over them, sending them to different functions and creating a new array with the new words}
